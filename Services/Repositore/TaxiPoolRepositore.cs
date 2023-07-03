38d7863c099cddaa1bcf186df8f40a0a5c3e4ad7
Evgenii Torov <59450111+midnight-code@users.noreply.github.com>
2023-07-03 16:37:05 +0300

    New fail

---
+++ Services/Repositore/TaxiPoolRepositore.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EL.BlackList.API.Services.Repositore
+{
+    public class TaxiPoolRepositore
+    {
+        private readonly Database _context;
+        public TaxiPoolRepositore(Database context)=>_context = context;
+    }
+}
